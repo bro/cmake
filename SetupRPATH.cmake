0e3a62564f9c05f73e795f5688dfd8b3de159f39
Christian Kreibich <christian@corelight.com>
2020-11-11 17:31:44 -0800

    Support libdir in SetupRPATH

--- SetupRPATH.cmake
+++ SetupRPATH.cmake
@@ -3,7 +3,7 @@
 # directory that were explicitly linked against
 if (NOT BINARY_PACKAGING_MODE)
     SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-    SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+    SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_FULL_LIBDIR}")
 
     include(GNUInstallDirs)
 
@@ -11,6 +11,6 @@ if (NOT BINARY_PACKAGING_MODE)
         # Ideally, we'd consistently use just one lib dir (e.g. lib/ or lib64/),
         # which requires every sub-project and external/embedded dependency
         # agrees and/or offers ability to install at that canonical location.
-        set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+        set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};${CMAKE_INSTALL_FULL_LIBDIR}")
     endif ()
 endif ()
